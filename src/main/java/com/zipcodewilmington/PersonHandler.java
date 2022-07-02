c124eee31f1a7df049911c8f356d4942c5f95ec4
Kyle Welch <welch@udel.edu>
2022-07-01 23:41:54 -0400

    passed tests

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -15,11 +15,14 @@ public class PersonHandler {
         // create a `counter`
         // while `counter` is less than length of array
             // begin loop
-
+        int counter = 0;
+        while (counter < personArray.length) {
+            result += personArray[counter];
+            counter++;
+        }
                 // use `counter` to identify the `current Person` in the array
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
-
             // end loop
         return result;
     }
@@ -31,14 +34,15 @@ public class PersonHandler {
         // identify initial value
         // identify terminal condition
         // identify increment
-
+        for (int i = 0 ; i < personArray.length ; i++) {
+            result += personArray[i];
+        }
         // use the above clauses to declare for-loop signature
             // begin loop
                 // use `counter` to identify the `current Person` in the array
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
             // end loop
-
         return result;
     }
 
@@ -48,13 +52,14 @@ public class PersonHandler {
         String result = "";
         // identify array's type
         // identify array's variable-name
-
+        for (Person thina : personArray) {
+            result += thina;
+        }
         // use the above discoveries to declare for-each-loop signature
             // begin loop
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
             // end loop
-
         return result;
     }
 
